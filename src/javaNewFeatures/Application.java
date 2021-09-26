b3367dbc2347377257ed26a144c0ea8aa7012c80
GeorgeMichaelGMIB <76493623+GeorgeMichaelGMIB@users.noreply.github.com>
2021-09-26 04:37:57 +0200

    Initializing project

---
+++ src/javaNewFeatures/Application.java
@@ -0,0 +1,14 @@
+package javaNewFeatures;
+
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        
+        
+	}
+
+}
